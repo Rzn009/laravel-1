f1b8d9175a3d6e9c1f5fbf8042882525a365631f
Razan Muhammad Aysel <razanmaysel08@gmail.com>
2024-02-12 23:55:03 -0800

    first commit

---
+++ resources/views/admin/parren.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="container w-full bg-cyan-400">
+        <h1 class="text-3xl font-bold text-center mt-10">
+            Hello world!
+        </h1>
+    </div>
+</body>
+
+</html>
